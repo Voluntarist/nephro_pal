e398f301a889b04fd4c2677cc443e90d84bfee42
Luke Walker <luke.t.walker@gmail.com>
2013-07-07 15:36:43 -0400

    having errors in my migration files ><

--- db/migrate/20130629191643_create_doctors.rb
+++ db/migrate/20130629191643_create_doctors.rb
@@ -3,6 +3,8 @@ class CreateDoctors < ActiveRecord::Migration
     create_table :doctors do |t|
       t.string :first_name
       t.string :last_name
+      t.integer :state_license_id
+      t.integer :insurance_license_id
       t.timestamps
     end
     add_index(:doctors, [:state_license_id, :insurance_license_id])
