7b322abf26cc14db31e8b1a009e740d9e015c57b
Luke Walker <luke.t.walker@gmail.com>
2013-07-08 17:24:54 -0400

    successfully created eligible doctor method / wrote tests to confirm method works.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,9 +5,15 @@ class Doctor < ActiveRecord::Base
   has_many :state_licenses, :through => :doctor_states, :uniq => true
   has_many :insurance_licenses, :through => :doctor_insurances, :uniq => true
 
+  scope :alphabetical, order('last_name ASC')
+  scope :has_state, lambda {|state_name| includes(:state_licenses).where("state_licenses.state_name" => state_name) }
+  scope :has_insurance, lambda {|insurance_name| includes(:insurance_licenses).where("insurance_licenses.insurance_name" => insurance_name) }
 
-  def self.eligible_doctors(state_name)
-    includes(:state_licenses).where("state_licenses.state_name" => state_name)
+
+  def self.eligible_doctors(args)
+    self.has_state(args[:state_name]).has_insurance(args[:insurance_name]).alphabetical
   end
+
+
 
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 describe Doctor do
+  let (:doctor){create(:doctor)}
+  let (:state_license){create(:state_license)}
+  let (:insurance_license){create(:insurance_license)}
+
   it { should respond_to(:first_name)}
   it { should respond_to(:last_name)}
-  it {should respond_to(:state_id)}
-  it {should respond_to(:license_id)}
   it {should respond_to(:state_licenses)}
+  it {should respond_to(:insurance_licenses)}
 
   it "has a valid factory" do
     create(:doctor).should be_valid
   end
 
   it 'does not add duplicate entries to state_id' do
-    doctor = create(:doctor)
-    state_license = create(:state_license)
     doctor.state_licenses << [state_license]
     doctor.state_licenses << [state_license]
     doctor.state_licenses.should == [state_license]
   end
 
   it 'adds two different state ids to a doctor' do
-    doctor = create(:doctor)
     state_license1 = create(:state_license)
     state_license2 = create(:state_license)
     doctor.state_licenses << [state_license1, state_license2]
@@ -27,14 +27,26 @@ describe Doctor do
   end
 
   describe 'class methods' do
+    describe 'verify the alphabetical doctor scope passes' do
+      it 'returns all the doctors in alphabetical order by last name' do
+        doc_s = create(:doctor, :last_name => "Smith")
+        doc_b = create(:doctor, :last_name => "Bonsib")
+        Doctor.find(:all).should_not == [doc_b, doc_s]
+        Doctor.alphabetical.should == [doc_b, doc_s]
+      end
+    end
     describe 'the #eligible_doctors method' do
       it 'finds an array of eligible doctors' do
         doc1 = create(:doctor)
         doc2 = create(:doctor)
+        doc3 = create(:doctor)
         state1 = create(:state_license, :state_name => 'Arkansas')
+        insurance1 = create(:insurance_license, :insurance_name => 'Blue Cross')
+        doc1.insurance_licenses << insurance1
         doc1.state_licenses << state1
         doc2.state_licenses << state1
-        Doctor.eligible_doctors('Arkansas').should == [doc1, doc2]
+        doc3.insurance_licenses << insurance1
+        Doctor.eligible_doctors(:state_name =>'Arkansas', :insurance_name => 'Blue Cross').should == [doc1]
       end
     end
   end
